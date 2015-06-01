bbf2f74c88b44f2b6b7e5782aa42f4b0586e0e9e
YaoyaoloveQiaoqiao <632212519@qq.com>
2015-06-01 21:41:26 +0800

    Third

---
+++ src/CRUD.java
@@ -0,0 +1,78 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class CRUD {
+	public static void main(String args[]) throws SQLException{
+		//read();
+		//create();
+		//update();
+		delete();
+	}
+	static void read() throws SQLException{
+		Connection conn=null;
+		Statement st=null;
+		ResultSet rs=null;
+		try {
+			
+		    conn=jdbcUtils.getConnection();
+			st=conn.createStatement();
+			rs=st.executeQuery("select stu_no,score from exam_student");
+			while(rs.next()){
+				System.out.println(rs.getObject("stu_no")+"\t"+rs.getObject("score"));
+			}
+		} finally{
+			jdbcUtils.free(rs, st, conn);
+		}
+	}
+     
+
+static void create() throws SQLException{
+	Connection conn=null;
+	Statement st=null;
+	ResultSet rs=null;
+	try {
+		
+	    conn=jdbcUtils.getConnection();
+		st=conn.createStatement();
+		String sql ="insert into exam_student(stu_no,score)values(95003,99)";
+		int i=st.executeUpdate(sql);
+		System.out.println("i="+i);
+	} finally{
+		jdbcUtils.free(rs, st, conn);
+	}
+}
+
+static void update() throws SQLException{
+	Connection conn=null;
+	Statement st=null;
+	ResultSet rs=null;
+	try {
+		
+	    conn=jdbcUtils.getConnection();
+		st=conn.createStatement();
+		String sql ="update exam_student set score=score+1";
+		int i=st.executeUpdate(sql);
+		System.out.println("i="+i);
+	} finally{
+		jdbcUtils.free(rs, st, conn);
+	}
+}
+static void delete() throws SQLException{
+	Connection conn=null;
+	Statement st=null;
+	ResultSet rs=null;
+	try {
+		
+	    conn=jdbcUtils.getConnection();
+		st=conn.createStatement();
+		String sql ="delete from exam_student where score=100";
+		int i=st.executeUpdate(sql);
+		System.out.println("i="+i);
+	} finally{
+		jdbcUtils.free(rs, st, conn);
+	}
+}
+}
---
+++ src/Read.java
@@ -0,0 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+
+
+
+public class Read extends JFrame{
+	public Read(String title){
+		//设置窗体属性
+		this.setSize(400 , 500);
+		this.setLocation(100, 100);
+		this.setTitle(title);
+		this.setFont(new Font("",Font.BOLD,40));
+		
+		//new一大堆组件
+		JButton btnSend = new JButton("发送") ;
+		
+		
+	
+		
+		//注册事件
+		
+		
+		//布置小面板
+		
+		panSmall.add(btnSend) ;
+		
+		//布置大面板
+	
+ 		
+		//布置窗体
+ 		this.setLayout(new BorderLayout());
+ 		
+ 		this.add(panBig, BorderLayout.NORTH) ;
+ 		this.add(panCon, BorderLayout.CENTER) ;
+ 		
+
+}
