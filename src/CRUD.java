7c7cbc17ca41e6bc7609fa612730ed5975049e4a
YaoyaoloveQiaoqiao <632212519@qq.com>
2015-06-03 22:55:39 +0800

    fifth

--- src/CRUD.java
+++ src/CRUD.java
@@ -7,84 +7,86 @@ import java.sql.Statement;
 
 import javax.swing.JFrame;
 
-
-
-
-public class CRUD extends JFrame  implements ActionListener {
-	public static void main(String args[]) throws SQLException{
+public class CRUD extends JFrame implements ActionListener {
+	public static void main(String args[]) throws SQLException {
 		//read();
-		//create();
-		//update();
-		delete();
+		// create();
+		// update();
+		// delete();
 	}
-	static void read() throws SQLException{
-		Connection conn=null;
-		Statement st=null;
-		ResultSet rs=null;
+
+	static void read(String read) throws SQLException {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		//String read = "select * from student";
+		
 		try {
-			
-		    conn=jdbcUtils.getConnection();
-			st=conn.createStatement();
-			rs=st.executeQuery("select stu_no,score from exam_student");
-			while(rs.next()){
-				System.out.println(rs.getObject("stu_no")+"\t"+rs.getObject("score"));
+
+			conn = jdbcUtils.getConnection();
+			st = conn.createStatement();
+			rs = st.executeQuery(read);
+			while (rs.next()) {
+				System.out.println(rs.getObject("snum") + "\t"
+						+ rs.getObject("sname"));
 			}
-		} finally{
+		} finally {
 			jdbcUtils.free(rs, st, conn);
 		}
 	}
-     
 
-static void create() throws SQLException{
-	Connection conn=null;
-	Statement st=null;
-	ResultSet rs=null;
-	try {
-		
-	    conn=jdbcUtils.getConnection();
-		st=conn.createStatement();
-		String sql ="insert into exam_student(stu_no,score)values(95003,99)";
-		int i=st.executeUpdate(sql);
-		System.out.println("i="+i);
-	} finally{
-		jdbcUtils.free(rs, st, conn);
-	}
-}
+	static void create() throws SQLException {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
 
-static void update() throws SQLException{
-	Connection conn=null;
-	Statement st=null;
-	ResultSet rs=null;
-	try {
-		
-	    conn=jdbcUtils.getConnection();
-		st=conn.createStatement();
-		String sql ="update exam_student set score=score+1";
-		int i=st.executeUpdate(sql);
-		System.out.println("i="+i);
-	} finally{
-		jdbcUtils.free(rs, st, conn);
+			conn = jdbcUtils.getConnection();
+			st = conn.createStatement();
+			String sql = "insert into exam_student(stu_no,score)values(95003,99)";
+			int i = st.executeUpdate(sql);
+			System.out.println("i=" + i);
+		} finally {
+			jdbcUtils.free(rs, st, conn);
+		}
 	}
-}
-static void delete() throws SQLException{
-	Connection conn=null;
-	Statement st=null;
-	ResultSet rs=null;
-	try {
-		
-	    conn=jdbcUtils.getConnection();
-		st=conn.createStatement();
-		String sql ="delete from exam_student where score=100";
-		int i=st.executeUpdate(sql);
-		System.out.println("i="+i);
-	} finally{
-		jdbcUtils.free(rs, st, conn);
-	}
-}
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	
-	}
-}
 
+	static void update() throws SQLException {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+
+			conn = jdbcUtils.getConnection();
+			st = conn.createStatement();
+			String sql = "update exam_student set score=score+1";
+			int i = st.executeUpdate(sql);
+			System.out.println("i=" + i);
+		} finally {
+			jdbcUtils.free(rs, st, conn);
+		}
+	}
+
+	static void delete() throws SQLException {
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+
+			conn = jdbcUtils.getConnection();
+			st = conn.createStatement();
+			String sql = "delete from exam_student where score=100";
+			int i = st.executeUpdate(sql);
+			System.out.println("i=" + i);
+		} finally {
+			jdbcUtils.free(rs, st, conn);
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
--- src/Read.java
+++ src/Read.java
@@ -2,44 +2,77 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 
 
 
-public class Read extends JFrame{
+public class Read extends JFrame implements ActionListener{
+
+	JTextField txtNum = new JTextField() ;
+	JTextField txtSex = new JTextField() ;
+	JTextField txtAddress = new JTextField() ;
+	
 	public Read(String title){
 		//设置窗体属性
 		this.setSize(400 , 500);
 		this.setLocation(100, 100);
 		this.setTitle(title);
-		this.setFont(new Font("",Font.BOLD,40));
+		
 		
 		//new一大堆组件
-		JButton btnSend = new JButton("发送") ;
-		
-		
+		JButton btnSend = new JButton("查询") ;
+		//JButton btnWrite = new JButton("录入") ;
 	
-		
 		//注册事件
+		btnSend.addActionListener(this); 
+		
+		
 		
 		
 		//布置小面板
 		JPanel panSmall=new JPanel();
+		panSmall.setLayout(new GridLayout(1,2));
 		panSmall.add(btnSend) ;
+		//panSmall.add(btnWrite);
+		panSmall.add(txtNum);
 		
-		//布置大面板
-	
- 		
+		
 		//布置窗体
  		this.setLayout(new BorderLayout());
  		
- 		this.add(panSmall, BorderLayout.NORTH) ;
+ 		this.add(panSmall, BorderLayout.CENTER) ;
  		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		//System.out.println("click");
+		if(e.getActionCommand().equals("查询")){
+			
+			
+			
+			String snum = txtNum.getText() ;
+			String read="select * from student where snum = " + snum;
+			CRUD r=new CRUD();
+			try {
+				r.read(read);
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
 	}	
 
 }
